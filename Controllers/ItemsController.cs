7bff5cabc7b280b5cb899780875b042fc9730d4d
Guest User <Guest@epicodus-9.local>
2018-04-19 10:13:21 -0700

    Add Categories linking to list

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,38 +1,27 @@
 using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
-
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-            List<Item> allItems = Item.GetAll();
-            return View(allItems);
-        }
-
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
-        {
-            return View();
-        }
-        [HttpPost("/items")]
-        public ActionResult Create()
-        {
-          Item newItem = new Item (Request.Form["new-item"]);
-          newItem.Save();
-          List<Item> allItems = Item.GetAll();
-          return View("Index", allItems);
-        }
-
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
-        {
-            Item.ClearAll();
-            return View();
-        }
+       [HttpGet("/categories/{categoryId}/items/new")]
+       public ActionResult CreateForm(int categoryId)
+       {
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Category category = Category.Find(categoryId);
+          return View(category);
+       }
+       [HttpGet("/categories/{categoryId}/items/{itemId}")]
+       public ActionResult Details(int categoryId, int itemId)
+       {
+          Item item = Item.Find(itemId);
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Category category = Category.Find(categoryId);
+          model.Add("item", item);
+          model.Add("category", category);
+          return View(item);
+       }
     }
 }
